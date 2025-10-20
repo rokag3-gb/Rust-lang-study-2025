a096cddd9aeba2157d28aa3159917d2ca7832f54
jwoo.kim <rokag3@gmail.com>
2025-10-20 17:31:07 +0900

    4.2 완료. 4.3 Slices 파일 추가

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod ch03_3_func;
 mod ch03_5_control_statement;
 mod ch04_1_ownership;
 mod ch04_2_references_borrowing;
+mod ch04_3_slices;
 
 fn main() {
     // ch02_1_guessing_game::main();
@@ -13,5 +14,6 @@ fn main() {
     // ch03_3_func::main();
     // ch03_5_control_statement::main();
     // ch04_1_ownership::main();
-    ch04_2_references_borrowing::main();
+    // ch04_2_references_borrowing::main();
+    ch04_3_slices::main();
 }
